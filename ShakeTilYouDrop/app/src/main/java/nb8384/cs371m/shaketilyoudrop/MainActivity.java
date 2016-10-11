49feec1f1471e6dc4ea2ab6f4d3e00e090515084
nathanielballinger <nathanielballinger@utexas.edu>
2016-10-10 23:25:18 -0500

    Added TextView for num shakes that I will modify in code

--- ShakeTilYouDrop/app/src/main/java/nb8384/cs371m/shaketilyoudrop/MainActivity.java
+++ ShakeTilYouDrop/app/src/main/java/nb8384/cs371m/shaketilyoudrop/MainActivity.java
@@ -2,12 +2,17 @@ package nb8384.cs371m.shaketilyoudrop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView numShakesText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numShakesText = (TextView) findViewById(numShakesText);
     }
 }
