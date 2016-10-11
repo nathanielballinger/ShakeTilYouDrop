021514aa0db8a81c7b2173118e600ebef8c1ae2b
nathanielballinger <nathanielballinger@utexas.edu>
2016-10-10 23:27:08 -0500

    Gave it ID

--- ShakeTilYouDrop/app/src/main/java/nb8384/cs371m/shaketilyoudrop/MainActivity.java
+++ ShakeTilYouDrop/app/src/main/java/nb8384/cs371m/shaketilyoudrop/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        numShakesText = (TextView) findViewById(numShakesText);
+        numShakesText = (TextView) findViewById(R.id.numShakesText);
     }
 }
